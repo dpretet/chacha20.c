d6056acc6a1561493b4233e83f840558ff1647dc
Damien Pretet <damien.pretet@me.com>
2022-11-26 14:21:02 +0100

    Add all tests, from quarter round up to text encryption

--- chacha20.c
+++ chacha20.c
@@ -37,7 +37,7 @@ void chacha20_inner_block(uint32_t * state) {
     chacha20_quarter((state+3),(state+4),(state+9),(state+14));
 }
 
-uint32_t reverse_to_word(uint32_t * data) {
+uint32_t reverse_to_dword(uint32_t * data) {
 
     uint32_t temp = 0;
     temp = *(data+3) << 24;
@@ -47,9 +47,8 @@ uint32_t reverse_to_word(uint32_t * data) {
     return temp;
 };
 
-void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce) {
+void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce, uint32_t * state) {
 
-    uint32_t state[16];
     uint32_t working_state[16];
     const uint32_t constant[4] = {0x61707865, 0x3320646e, 0x79622d32, 0x6b206574};
 
@@ -58,38 +57,38 @@ void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce) {
     state[2] = constant[2];
     state[3] = constant[3];
 
-    state[4] = reverse_to_word(key+0);
-    state[5] = reverse_to_word(key+4);
-    state[6] = reverse_to_word(key+8);
-    state[7] = reverse_to_word(key+12);
-    state[8] = reverse_to_word(key+16);
-    state[9] = reverse_to_word(key+20);
-    state[10] = reverse_to_word(key+24);
-    state[11] = reverse_to_word(key+28);
+    state[4] = reverse_to_dword(key+0);
+    state[5] = reverse_to_dword(key+4);
+    state[6] = reverse_to_dword(key+8);
+    state[7] = reverse_to_dword(key+12);
+    state[8] = reverse_to_dword(key+16);
+    state[9] = reverse_to_dword(key+20);
+    state[10] = reverse_to_dword(key+24);
+    state[11] = reverse_to_dword(key+28);
 
     state[12] = *counter;
 
-    state[13] = reverse_to_word(nonce+0);
-    state[14] = reverse_to_word(nonce+4);
-    state[15] = reverse_to_word(nonce+8);
+    state[13] = reverse_to_dword(nonce+0);
+    state[14] = reverse_to_dword(nonce+4);
+    state[15] = reverse_to_dword(nonce+8);
 
     for (int i=0;i<16;i++) {
         working_state[i] = state[i];
     }
     
-    printf("ChaCha state with the key setup\n");
-    for (int i=0;i<16;i++) {
-        printf("%x\n", state[i]);
-    }
+    // printf("ChaCha state with the key setup\n");
+    // for (int i=0;i<16;i++) {
+        // printf("%x\n", state[i]);
+    // }
 
     for (int i=0;i<10;i++) {
         chacha20_inner_block(working_state);
     }
 
-    printf("ChaCha state after 20 rounds\n");
-    for (int i=0;i<16;i++) {
-        printf("%x\n", working_state[i]);
-    }
+    // printf("ChaCha state after 20 rounds\n");
+    // for (int i=0;i<16;i++) {
+        // printf("%x\n", working_state[i]);
+    // }
 
     for (int i=0;i<16;i++) {
         state[i] += working_state[i];
@@ -101,3 +100,14 @@ void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce) {
     }
 
 };
+
+void chacha20_serialize(uint32_t * block, char * serial) {
+
+    for (int i=0;i<16;i++) {
+        serial[i*4+0] = (block[i] & 0xFF);
+        serial[i*4+1] = ((block[i] >> 8) & 0xFF);
+        serial[i*4+2] = ((block[i] >> 16) & 0xFF);
+        serial[i*4+3] = ((block[i] >> 24) & 0xFF);
+    }
+
+}
--- chacha20.h
+++ chacha20.h
@@ -8,7 +8,13 @@
 
 void chacha20_quarter(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d);
 
-void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce);
+void chacha20_inner_block(uint32_t * state);
+
+uint32_t reverse_to_dword(uint32_t * data);
+
+void chacha20_block(uint32_t * key, uint32_t * counter, uint32_t * nonce,  uint32_t * state);
+
+void chacha20_serialize(uint32_t * block, char * serial);
 
 #endif
 
